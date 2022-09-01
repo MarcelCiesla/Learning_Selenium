fd5a5867e544cbb8e43fd2c8fa76e513574356eb
Stiku2137 <mciesla97@gmail.com>
2022-09-01 12:30:36 +0200

    webdriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,16 +1,43 @@
+import org.openqa.selenium.InvalidArgumentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
 
-    public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
+    @Test
+    public void openGooglePage() {
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Marcel\\Desktop\\programowanie\\chromedriver\\chromedriver.exe");
+        //System.setProperty("webdriver.gecko.driver", "C:\\Users\\Marcel\\Desktop\\programowanie\\firefoxdriver\\geckodriver.exe");
+        //System.setProperty("webdriver.ie.driver", "C:\\Users\\Marcel\\Desktop\\programowanie\\internetexplorerdriver\\IEDriverServer.exe");
+        //WebDriver driver = new InternetExplorerDriver();
+        //driver.get("https://www.google.com");
+
+        WebDriver driver = getDriver("ie");
+        driver.get("https://www.google.com");
     }
 
-    @Test
-    public void sampleTest() {
-        System.out.println("Hello");
+    public WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "chrome":
+                String chromePath = "C:\\Users\\Marcel\\Desktop\\programowanie\\chromedriver\\chromedriver.exe";
+                System.setProperty("webdriver.chrome.driver", chromePath);
+                return new ChromeDriver();
+            case "firefox":
+                String firefoxPath = "C:\\Users\\Marcel\\Desktop\\programowanie\\firefoxdriver\\geckodriver.exe";
+                System.setProperty("webdriver.gecko.driver", firefoxPath);
+                return new FirefoxDriver();
+            case "ie":
+                String iePath = "C:\\Users\\Marcel\\Desktop\\programowanie\\internetexplorerdriver\\IEDriverServer.exe";
+                System.setProperty("webdriver.ie.driver", iePath);
+                return new InternetExplorerDriver();
+            default:
+                throw new InvalidArgumentException("Invalid browser name");
+        }
+
+
     }
 
 }
