e0ce6e876e5b08691b1e19ec0b218469ced2d158
Stiku2137 <mciesla97@gmail.com>
2022-10-05 21:23:38 +0200

    xpath

---
+++ src/test/java/SubstrinTest.java
@@ -0,0 +1,11 @@
+public class SubstrinTest {
+
+    public static void main(String[] args) {
+        // w javie numerowane od 0 w xpathie od 1
+        String test = "username";
+        String substring = test.substring(test.length()-4);
+        System.out.println(substring);
+
+
+    }
+}
